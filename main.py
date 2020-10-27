60226e7dd1eaec13b774e1d126d5e2f55a53137d
Maffey <matiusz98@gmail.com>
2020-10-27 23:47:44 +0100

    further experimenting and creating a backbone of the app

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import kivy
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 
 # Ensures a proper version of kivy is installed.
 kivy.require('1.11.1')
 
 # Changes the rendered size of the window.
-Window.size = (500, 32)
+# Window.size = (500, 32)
+
+
+class MainMenu(Widget):
+    detection = ObjectProperty(None)
+    escalation = ObjectProperty(None)
+    penetration = ObjectProperty(None)
 
 
 class ChangeMAC(Widget):
@@ -17,7 +24,7 @@ class ChangeMAC(Widget):
 class SpearkyApp(App):
 
     def build(self):
-        menu_window = ChangeMAC()
+        menu_window = MainMenu()
         return menu_window
 
 
