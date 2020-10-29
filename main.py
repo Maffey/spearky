02f83c273cef1b76706a3d68d1595da68316d580
Maffey <matiusz98@gmail.com>
2020-10-29 01:17:56 +0100

    implemented ScreenManager

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import kivy
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 
 # Ensure a proper version of kivy is installed.
 kivy.require('1.11.1')
 
 
-class MainMenu(Widget):
-    detection = ObjectProperty(None)
-    escalation = ObjectProperty(None)
-    penetration = ObjectProperty(None)
+# The WindowManager class is responsible for properly changing Screens in the app.
+class WindowManager(ScreenManager):
+    pass
 
 
-class ChangeMAC(Widget):
+class MainMenuScreen(Screen):
+    pass
+
+
+class ChangeMACScreen(Screen):
     # Initialize widgets of the class taken from .kv file.
     mac_input = ObjectProperty(None)
 
-    def submit_mac_callback(self):
+    def submit_mac(self):
         print(f"Your MAC address: {self.mac_input.text}")
         self.mac_input.text = ""
 
 
 class SpearkyApp(App):
-
-    def build(self):
-        menu_window = ChangeMAC()
-        return menu_window
+    pass
 
 
 if __name__ == '__main__':
